633577a2c14c3364797a2c6424ba11345fbb10b2
Santiago Cuervo <cuervo.santiago@gmail.com>
2025-04-20 15:57:16 -0500

    Update LoanRepository with method to retrieve loans for traversal

--- src/main/java/me/santiagocuervo/LoanRepository.java
+++ src/main/java/me/santiagocuervo/LoanRepository.java
@@ -1,9 +1,13 @@
 package me.santiagocuervo;
 
+import java.util.List;
+
 public interface LoanRepository {
 
     void save(Loan loan);
 
     Loan findById(String id);
+
+    List<Loan> getLoans();
 
 }
