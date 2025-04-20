0d8832227b4d5f980d4cd426124f0511e56cd3ae
Santiago Cuervo <cuervo.santiago@gmail.com>
2025-04-20 14:24:38 -0500

    Add testBorrowNonExistingBook

---
+++ src/test/java/me/santiagocuervo/LibraryServiceTest.java
@@ -0,0 +1,33 @@
+package me.santiagocuervo;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class LibraryServiceTest {
+
+    @Mock
+    private BookRepository bookRepository;
+    @Mock
+    private LoanRepository loanRepository;
+    private LibraryService libraryService;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+        libraryService = new LibraryService(bookRepository, loanRepository);
+    }
+
+    @Test
+    void testBorrowNonExistingBook() {
+        String userId = "1234";
+        String bookId = "5678";
+
+        assertThrows(NoSuchElementException.class, () -> libraryService.borrowBook(userId, bookId));
+    }
+
+}
